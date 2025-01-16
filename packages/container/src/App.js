95e59bbe4474574fa5a53637ec3d6aaea2aa9f01
Sachin568-Gupta <gsachin568@gmail.com>
2025-01-16 19:14:25 +0530

    auth module added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,20 +1,34 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import MarketingApp from "./components/MarketingApp";
+import React, { lazy, Suspense, useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
-import { createGenerateClassName, StylesProvider } from "@material-ui/core/styles";
+import {
+  createGenerateClassName,
+  StylesProvider,
+} from "@material-ui/core/styles";
+import Progress from "./components/Progress";
+
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
 export default () => {
+  const [isSignin, setInSignin] = useState(false);
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
-          <MarketingApp />
+          <Header isSignin={isSignin} onSignOut={() => setInSignin(false)} />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setInSignin(true)} />
+              </Route>
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
